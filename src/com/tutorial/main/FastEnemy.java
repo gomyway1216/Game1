f23293962941dc8c139c749bb8ddeb31be337f96
gomyway1216 <uwyudai@gmail.com>
2020-05-19 15:52:11 -0700

    created smart enemy.

--- src/com/tutorial/main/FastEnemy.java
+++ src/com/tutorial/main/FastEnemy.java
@@ -7,7 +7,7 @@ import java.awt.Rectangle;
 public class FastEnemy extends GameObject {
 	private Handler handler;
 	
-	public FastEnemy(int x, int y, ID id, Handler handler) {
+	public FastEnemy(float x, float y, ID id, Handler handler) {
 		super(x, y, id);
 		
 		this.handler = handler;
@@ -17,7 +17,7 @@ public class FastEnemy extends GameObject {
 	}
 	
 	public Rectangle getBounds() {
-		return new Rectangle(x, y, 16, 16);
+		return new Rectangle((int)x, (int)y, 16, 16);
 	}
 
 	@Override
@@ -39,6 +39,6 @@ public class FastEnemy extends GameObject {
 	@Override
 	public void render(Graphics g) {
 		g.setColor(Color.CYAN);		
-		g.fillRect(x, y, 16, 16);
+		g.fillRect((int)x, (int)y, 16, 16);
 	}
 }
--- src/com/tutorial/main/Player.java
+++ src/com/tutorial/main/Player.java
@@ -9,13 +9,13 @@ public class Player extends GameObject {
 	Random r = new Random();
 	Handler handler;
 
-	public Player(int x, int y, ID id, Handler handler) {
+	public Player(float x, float y, ID id, Handler handler) {
 		super(x, y, id);
 		this.handler = handler;
 	}
 		
 	public Rectangle getBounds() {
-		return new Rectangle(x, y, 32, 32);
+		return new Rectangle((int)x, (int)y, 32, 32);
 	}
 
 	@Override
@@ -36,7 +36,7 @@ public class Player extends GameObject {
 		for(int i = 0; i < handler.object.size(); i++) {
 			GameObject tempObject = handler.object.get(i);
 			
-			if(tempObject.getId() == ID.BasicEnemy || tempObject.getId() == ID.FastEnemy) {
+			if(tempObject.getId() == ID.BasicEnemy || tempObject.getId() == ID.FastEnemy || tempObject.getId() == ID.SmartEnemy) {
 				// collision code
 				if(getBounds().intersects(tempObject.getBounds())) {
 					HUD.HEALTH -= 2;
@@ -48,6 +48,6 @@ public class Player extends GameObject {
 	@Override
 	public void render(Graphics g) {
 		g.setColor(Color.white);
-		g.fillRect(x, y, 32, 32);
+		g.fillRect((int)x, (int)y, 32, 32);
 	}
 }
