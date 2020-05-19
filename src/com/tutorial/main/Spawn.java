d1c35cc0eade3a9204c15bd788c8b4246ef1f24c
gomyway1216 <uwyudai@gmail.com>
2020-05-19 14:56:04 -0700

    created enemies based on levels.

--- src/com/tutorial/main/Spawn.java
+++ src/com/tutorial/main/Spawn.java
@@ -1,8 +1,11 @@
 package com.tutorial.main;
 
+import java.util.Random;
+
 public class Spawn {
 	private Handler handler;
 	private HUD hud;
+	private Random r = new Random();
 	
 	private int scoreKeep = 0;
 	
@@ -14,9 +17,18 @@ public class Spawn {
 	public void tick() {
 		scoreKeep++;
 		
-		if(scoreKeep >= 1000) {
+		if(scoreKeep >= 250) {
 			scoreKeep = 0;
 			hud.setLevel(hud.getLevel() + 1);
+			
+			if(hud.getLevel() == 2) {
+				handler.addObject(new BasicEnemy(r.nextInt(Game.WIDTH - 50), r.nextInt(Game.HEIGHT - 50), ID.BasicEnemy, handler));
+			} else if(hud.getLevel() == 3) {
+				handler.addObject(new BasicEnemy(r.nextInt(Game.WIDTH - 50), r.nextInt(Game.HEIGHT - 50), ID.BasicEnemy, handler));
+				handler.addObject(new BasicEnemy(r.nextInt(Game.WIDTH - 50), r.nextInt(Game.HEIGHT - 50), ID.BasicEnemy, handler));
+			} else if(hud.getLevel() == 4) {
+				handler.addObject(new FastEnemy(r.nextInt(Game.WIDTH - 50), r.nextInt(Game.HEIGHT - 50), ID.BasicEnemy, handler));
+			}
 		}
 	}
 }
